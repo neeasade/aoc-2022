5dcdafbbd4ece8b9588cb76f557d6e15e01c40d4
neeasade <neeasade@users.noreply.github.com>
2022-12-04 12:23:54 -0600

    day one

--- milk and cookies.clj
+++ milk and cookies.clj
@@ -11,3 +11,13 @@
 ;; then: (cider-connect-clj '(:host "localhost" :port 1667))
 
 ;; day 1
+
+(let [in (slurp "inputs/1.txt")]
+  (->> (string/split in #"\n\n")
+       (map (fn [parts]
+              (map read-string (string/split parts #"\n"))))
+       (map (partial apply +))
+       (sort >)
+       ;; (first)                       ; part 1
+       (take 3)
+       (apply +)))                      ; part 2
